ca7b8ee31f926002c5e8a204eaa7a8810186ecd6
matt venn <matt@dygma.com>
2019-06-09 16:35:03 +0200

    simple counter

---
+++ counter/counter.v
@@ -0,0 +1,16 @@
+module counter (input clk);
+    reg [15:0] counter;
+
+    initial counter=0;
+
+    always @(posedge clk)
+        if(counter==16'd22)
+            counter<=0;
+        else
+            counter<=counter+1'b1;
+
+    `ifdef FORMAL
+    always @(*)
+        assert(counter!=16'd500);
+    `endif
+endmodule
