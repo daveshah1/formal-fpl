0e12356d17ea6a842ba7b0640515b1c7cc531a23
matt venn <matt@mattvenn.net>
2019-06-12 10:44:54 +0200

    name change

--- double-shift-reg/double_shift_reg.v
+++ double-shift-reg/double_shift_reg.v
@@ -1,5 +1,5 @@
 `default_nettype	none
-module dblfifo(i_clk, i_ce, i_data, o_data);
+module double_shift_reg(i_clk, i_ce, i_data, o_data);
 	input	wire	i_clk, i_ce;
 	
 	input	wire	i_data;
@@ -7,8 +7,8 @@ module dblfifo(i_clk, i_ce, i_data, o_data);
 
 	wire	a_data, b_data;
 
-	fifo	one(i_clk, 1'b0, i_ce, i_data, a_data);
-	fifo	two(i_clk, 1'b0, i_ce, i_data, b_data);
+	shift_reg	a(i_clk, 1'b0, i_ce, i_data, a_data);
+	shift_reg	b(i_clk, 1'b0, i_ce, i_data, b_data);
 
 	initial	o_data = 1'b0;
 	always @(posedge i_clk)
@@ -16,5 +16,6 @@ module dblfifo(i_clk, i_ce, i_data, o_data);
 
     `ifdef FORMAL
         // your properties here
+        always @(*) assert(!o_data);
     `endif
 endmodule
